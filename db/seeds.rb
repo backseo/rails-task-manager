3ec1770c3df84ed8f91f1c093822597dde07dfeb
serge cleoron <sc21417@gmail.com>
2017-11-07 16:33:41 +0100

    task manager without css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+task_attributes = [
+{title: 'Nettoyage', content: 'Faire les vitres, laver les sols, sortir les poubelles'},
+{title: 'Pharmacie', content: 'Acheter des dolipranes pour Philippe'},
+{title: 'Courses', content: 'Acheter des pepitos'}
+]
+
+task_attributes.each { |task_attrib| Task.create! task_attrib }
